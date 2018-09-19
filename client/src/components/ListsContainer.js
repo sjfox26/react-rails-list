9f5c9543598530ff87c85ba58cdaf6254ce93999
Scott Fox <scottfox87@gmail.com>
2018-09-19 15:13:42 -0500

    refactor separate lists with a List component

--- client/src/components/ListsContainer.js
+++ client/src/components/ListsContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import List from './List';
 
 class ListsContainer extends Component {
     constructor(props){
@@ -22,12 +23,7 @@ class ListsContainer extends Component {
         return (
             <div className="lists-container">
                 {this.state.lists.map( list => {
-                    return (
-                        <div className="single-list" key={list.id}>
-                            <h4>{list.title}</h4>
-                            <p>{list.excerpt}</p>
-                        </div>
-                    )
+                    return (<List list={list} key={list.id} />)
                 })}
             </div>
         )
