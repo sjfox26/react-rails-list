9994bba9a803542396dcc4b66673f66cc6a1f335
Scott Fox <scottfox87@gmail.com>
2018-09-19 15:26:57 -0500

    add post/create functionality to front-end

--- client/src/components/ListsContainer.js
+++ client/src/components/ListsContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import List from './List';
+import NewListForm from './NewListForm';
 
 class ListsContainer extends Component {
     constructor(props){
@@ -19,12 +20,26 @@ class ListsContainer extends Component {
             })
             .catch(error => console.log(error))
     }
+
+    addNewList(title, excerpt) {
+        axios.post( '/api/v1/lists', { list: {title, excerpt} })
+            .then(response => {
+                console.log(response)
+                const lists = [ ...this.state.lists, response.data ]
+                this.setState({lists})
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     render() {
         return (
             <div className="lists-container">
                 {this.state.lists.map( list => {
                     return (<List list={list} key={list.id} />)
                 })}
+                <NewListForm onNewList={this.addNewList} />
             </div>
         )
     }
